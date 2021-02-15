64624ff2b2d60f760e4a7eb035e7d04debe38bc9
tmanohara <thisaramanohara@gmail.com>
2021-02-16 01:43:09 +0530

    eating red ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,10 +65,51 @@ export class AppComponent {
 
         return (n && n1);
       }
+      /*
+      //eating from middle
+      else if(id==='90' || id==='91' || id==='92' || id==='93' ||id==='94' ||id==='95' ||id==='96' ||id==='97' ||id==='98' ||id==='99'){
+        let left=false
+        let right=false
+
+
+
+
+
+        return [left,right]
+      }
+      */
 
 
     }
   }
+
+  eatRedStartedFromMiddle_forLeft(id:string){
+    //can eat for leftside
+    const col=document.getElementById((parseInt(id)-11).toString());
+    const www:any=col?.getAttribute("style");
+    const n = www.includes("background-color: red");
+
+    const col1=document.getElementById((parseInt(id)-22).toString());
+    const www1:any=col1?.getAttribute("style");
+    const n1 = www1.includes("background-color: #d3d34f");
+
+    return (n && n1);
+  }
+
+  eatRedStartedFromMiddle_forRight(id:string){
+    //can eat for rightside
+    const col2=document.getElementById((parseInt(id)-9).toString());
+    const www2:any=col2?.getAttribute("style");
+    const n2 = www2.includes("background-color: red");
+
+    const col12=document.getElementById((parseInt(id)-18).toString());
+    const www12:any=col12?.getAttribute("style");
+    const n12 = www12.includes("background-color: #d3d34f");
+
+    return (n2 && n12);
+  }
+
+
 
   goToWhite(clickedId:string,nextId:string){
     const col=document.getElementById(nextId);
@@ -99,6 +140,17 @@ export class AppComponent {
           let fff=document.getElementById((parseInt(clickedId)-11).toString());
           //let qqq:any=fff?.getAttribute("style");
           fff?.setAttribute("style","background-color: #d3d34f;");
+        }
+        else if(this.eating && this.eatingFromMiddle){
+          if(parseInt(nextId)>parseInt(clickedId)-20){
+            let fff=document.getElementById((parseInt(clickedId)-9).toString());
+          //let qqq:any=fff?.getAttribute("style");
+            fff?.setAttribute("style","background-color: #d3d34f;");
+          }else{
+            let fff=document.getElementById((parseInt(clickedId)-11).toString());
+            //let qqq:any=fff?.getAttribute("style");
+            fff?.setAttribute("style","background-color: #d3d34f;");
+          }
         }
 
 
@@ -312,8 +364,41 @@ export class AppComponent {
     //when bott0m edges are clicked
     else if(id==='90' || id==='91' || id==='92' || id==='93' ||id==='94' ||id==='95' ||id==='96' ||id==='97' ||id==='98' ||id==='99'){
       a?.setAttribute("style","background-color: black;");
-      let v = parseInt(id)-11;
-      let w = parseInt(id)-9;
+
+      let left= this.eatRedStartedFromMiddle_forLeft(id);
+      let right=this.eatRedStartedFromMiddle_forRight(id);
+
+
+
+      let v;
+      let w;
+      if(left && !right){
+        this.eating=true;
+        this.eatingFromRight=false;
+        this.eatingFromLeft=false;
+        this.eatingFromMiddle=true;
+        v = parseInt(id)-22;
+        w = parseInt(id)-9;
+      }else if(right && !left){
+        this.eating=true;
+        this.eatingFromRight=false;
+        this.eatingFromLeft=false;
+        this.eatingFromMiddle=true;
+        v = parseInt(id)-11;
+        w = parseInt(id)-18;
+      }else if(left && right){
+        this.eating=true;
+        this.eatingFromRight=false;
+        this.eatingFromLeft=false;
+        this.eatingFromMiddle=true;
+        v = parseInt(id)-22;
+        w = parseInt(id)-18;
+      }else{
+        v = parseInt(id)-11;
+        w = parseInt(id)-9;
+      }
+
+
       //let nextId = toString(parseInt(id) - 10 + 1)
       this.currentElements.push(v);
       this.currentElements.push(w);
@@ -327,6 +412,7 @@ export class AppComponent {
       c?.setAttribute("style","background-color: white;");
     }
 
+      this.currentElements.length=0;
 
       //console.log(b)
     }
@@ -335,8 +421,41 @@ export class AppComponent {
     //if(id==='90' || id==='91' || id==='92' || id==='93' ||id==='94' ||id==='95' ||id==='96' ||id==='97' ||id==='98' ||id==='99'){
     else {
       a?.setAttribute("style","background-color: black;");
-      let v = parseInt(id)-11;
-      let w = parseInt(id)-9;
+
+      let left= this.eatRedStartedFromMiddle_forLeft(id);
+      let right=this.eatRedStartedFromMiddle_forRight(id);
+
+
+
+
+      let v;
+      let w;
+      if(left && !right){
+        this.eating=true;
+        this.eatingFromRight=false;
+        this.eatingFromLeft=false;
+        this.eatingFromMiddle=true;
+        v = parseInt(id)-22;
+        w = parseInt(id)-9;
+      }else if(right && !left){
+        this.eating=true;
+        this.eatingFromRight=false;
+        this.eatingFromLeft=false;
+        this.eatingFromMiddle=true;
+        v = parseInt(id)-11;
+        w = parseInt(id)-18;
+      }else if(left && right){
+        this.eating=true;
+        this.eatingFromRight=false;
+        this.eatingFromLeft=false;
+        this.eatingFromMiddle=true;
+        v = parseInt(id)-22;
+        w = parseInt(id)-18;
+      }else{
+        this.eating=false;
+        v = parseInt(id)-11;
+        w = parseInt(id)-9;
+      }
       //let nextId = toString(parseInt(id) - 10 + 1)
       const b = document.getElementById(v.toString())
       //console.log(typeof(b))
@@ -352,6 +471,7 @@ export class AppComponent {
         c?.setAttribute("style","background-color: white;");
       }
       //console.log(b)
+      this.currentElements.length=0;
     }
 
 
